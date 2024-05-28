d7e744c9319d59230bb22eefbbb490a234ca7b3f
Miladshahhi <Miladshahhi@users.noreply.github.com>
2024-05-28 00:14:25 +0000

    ✅ Ready to clone and code.

--- miladshahhi/ext/auth.py
+++ miladshahhi/ext/auth.py
@@ -1,8 +1,8 @@
 from flask_simplelogin import SimpleLogin
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from project_name.ext.database import db
-from project_name.models import User
+from miladshahhi.ext.database import db
+from miladshahhi.models import User
 
 
 def verify_login(user):
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import sys
 import pytest
 
-from project_name import create_app
-from project_name.ext.commands import populate_db
-from project_name.ext.database import db
+from miladshahhi import create_app
+from miladshahhi.ext.commands import populate_db
+from miladshahhi.ext.database import db
 
 
 @pytest.fixture(scope="session")
